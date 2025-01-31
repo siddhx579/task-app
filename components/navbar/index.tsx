092f58a283cfd638d9eec560234f5bde37113d9a
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-01 01:03:56 +0530

    Create index.tsx

---
+++ components/navbar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TeamSwitcher from './team-switcher'
+import Links from './links'
+import Search from './search'
+import Avatar from './avatar'
+
+export default function Navbar() {
+    return (
+        <nav className='flex justify-between items-center px-4 py-3'>
+            <div className='flex items-center gap-12 '>
+                <TeamSwitcher />
+                <Links />
+            </div>
+            <div className="flex items-center gap-4">
+                <Search />
+                <Avatar />
+            </div>
+        </nav>
+    )
+}
