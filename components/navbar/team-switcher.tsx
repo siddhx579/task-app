2a2ef44a5e2a57bb643108fa8a2452c12b6192d8
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-01 01:03:59 +0530

    Create team-switcher.tsx

---
+++ components/navbar/team-switcher.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '../ui/command'
+import { PlusCircleIcon } from 'lucide-react'
+
+const groups = [
+    {
+        label: "Personal Account",
+        teams: [{ label: "Siddhant K." }],
+    },
+    {
+        label: "Teams",
+        teams: [
+            { label: "Siddhant V1." },
+            { label: "Siddhant V2." },
+            { label: "Siddhant V3." },
+        ],
+    },
+];
+
+export default function TeamSwitcher() {
+    return (
+        <Popover>
+            <PopoverTrigger>
+                <Button variant="outline" role='combobox' aria-label='Select a team' className='w-[200px] justify-between'>
+                    <Avatar className='mr-2 h-6 w-6'>
+                        <AvatarImage src='http://avatar.vercel.sh/Siddhant.png' alt='Avatar' />
+                        <AvatarFallback>EV</AvatarFallback>
+                    </Avatar>
+                    Siddhant Kapoor
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className='w-80'>
+                <Command>
+                    <CommandInput placeholder="Search Term..." />
+                    <CommandList>
+                        <CommandEmpty>No team found</CommandEmpty>
+                        {groups.map((group) => (
+                            <CommandGroup key={group.label} heading={group.label}>
+                                {group.teams.map((team) => (
+                                    <CommandItem key={team.label} className="text-sm">
+                                        <Avatar className="mr-2 h-5 w-5">
+                                            <AvatarImage alt={team.label} src={`https://avatar.vercel.sh/${team.label}.png`} />
+                                            <AvatarFallback className="uppercase">
+                                                {team.label}
+                                            </AvatarFallback>
+                                        </Avatar>
+                                        {team.label}
+                                    </CommandItem>
+                                ))}
+                            </CommandGroup>
+                        ))}
+                    </CommandList>
+                    <CommandSeparator />
+                    <CommandList>
+                        <CommandGroup>
+                            <CommandItem>
+                                <PlusCircleIcon className="mr-2 h-5 w-5" />
+                                Create Team
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
+}
