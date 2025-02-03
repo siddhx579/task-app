6a2629d6ebed5568653c11a4ed4cb11951d9f709
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-04 00:26:14 +0530

    add toast

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,23 +1,38 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import { Form as FormComp, FormControl, FormField, FormItem, FormMessage } from "../ui/form";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
-import formSchema from './schema';
+import formSchema, { FormSchema } from './schema';
 import { Input } from '../ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 import StatusBullet from '../StatusBullet';
 import { Textarea } from '../ui/textarea';
 import { Button } from '../ui/button';
+import { IoAddOutline } from "react-icons/io5";
+import { createTask } from '@/services/task';
+import { VscLoading } from "react-icons/vsc";
+import { useToast } from '@/hooks/use-toast';
 
 export default function Form() {
     const form = useForm({
         resolver: zodResolver(formSchema),
     });
 
+    const { toast } = useToast();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = async (data: FormSchema) => {
+        setIsLoading(true);
+        await createTask(data);
+        setIsLoading(false);
+        toast({title: "Your new task was created successfully!"});
+    }
+
     return (
         <FormComp {...form}>
-            <form className="space-y-2">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
                 <div className="flex items-center gap-3">
                     <FormField control={form.control} name="title" render={({ field }) => (
                         <FormItem className="grow">
@@ -50,7 +65,7 @@ export default function Form() {
                             </Select>
                         </FormItem>
                     )} />
-                    <Button>
+                    <Button type="submit" icon={isLoading ? (<VscLoading className="animate-spin" />) : (<IoAddOutline />)}>
                         Add Task
                     </Button>
                 </div>
