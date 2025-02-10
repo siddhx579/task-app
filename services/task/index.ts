02ec613aa521b6cb06b15dc4570c387ce4c35e7b
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-11 00:10:51 +0530

    Update index.ts

--- services/task/index.ts
+++ services/task/index.ts
@@ -2,6 +2,7 @@
 
 import { FormSchema } from "@/components/form/schema";
 import prisma from "@/lib/prisma";
+import { Task } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 function revalidatePageData(){
@@ -24,4 +25,28 @@ export async function getTasks() {
         { createdAt: "desc"}
     })
     return tasks;
+}
+
+export async function deleteTask(id: string){
+    await prisma.task.delete({
+        where: { id },
+    });
+    revalidatePageData();
+}
+
+export async function updateTask(task: Task){
+    await prisma.task.update({
+        where: { id: task.id },
+        data: task,
+    });
+    revalidatePageData();
+}
+
+export async function getTaskCountByStatus() {
+    const [starting, progress, done] = await Promise.all([
+        prisma.task.count({ where: { status: "starting" }}),
+        prisma.task.count({ where: { status: "progress" }}),
+        prisma.task.count({ where: { status: "done" }}),
+    ])
+    return { starting, progress, done };
 }
