4caa0f955cb02fc464358e6ebe124327be1e85b8
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-10 17:43:43 +0530

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -42,12 +42,13 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, icon, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, icon, type, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }), "flex gap-2")}
         ref={ref}
+        type={type ? type : "button"}
         {...props}
       >
         {props.children}
