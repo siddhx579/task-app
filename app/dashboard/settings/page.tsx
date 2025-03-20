75ce3e78a519f2bdb50b2888f876952b7d88f42d
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-20 10:56:57 +0530

    Create page.tsx

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/navbar';
+import React from 'react'
+import Settings from './Settings';
+
+function page() {
+    return (
+        <div>
+            <Navbar />
+            <Settings />
+        </div>
+    )
+}
+
+export default page;
